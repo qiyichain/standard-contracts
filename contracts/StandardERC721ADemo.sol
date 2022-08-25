449e7af5f1ab0bf159dd47d2e05fc5aa95f29db9
yqq <youngqqcn@gmail.com>
2022-08-25 11:45:57 +0800

    fix contract name

--- contracts/StandardERC721ADemo.sol
+++ contracts/StandardERC721ADemo.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./StandardERC721A.sol";
 
-contract StandardERC721Demo is StandardERC721A  {
+contract StandardERC721ADemo is StandardERC721A  {
     constructor() ERC721A("StandardERC721ADemo", "STD721A") {
         baseTokenURI = "https://www.qiyichain.com/";
     }
