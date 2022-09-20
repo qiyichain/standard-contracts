34c0c6494e9ea34bee762ff864dcc00f213ddc45
yqq <youngqqcn@gmail.com>
2022-09-20 19:08:58 +0800

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -30,8 +30,7 @@ module.exports = {
   defaultNetwork: "qiyichain",
   networks: {
     qiyichain: {
-      url: "http://172.16.100.103:8545", // 线上
-      // url: "http://192.168.110.37:8545", // 本地
+    //   url: "http://172.16.100.103:8545", // 测试网
       accounts: accounts,
       chainId: 12285, // We set 1337 to make interacting with MetaMask simpler
       gas: 40000000,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -41,30 +41,57 @@ async function main() {
     let tx2 = await didProxy.transferOwnership("0xC2ED445Ee8DC5791662C574CE21dA57Ba5F2c79f")
     await expect((await tx2.wait()).status).equal(1)
 
+    // 0x8054b6F9EDf88b36BE2431f01E8a3D7fF433252c  // 代理合约
+    // 0xCCb632cdC419500015bc3fDad6335e348f007a76  // 手续费
+    // tx = await addressList.addDeveloper('0x8054b6F9EDf88b36BE2431f01E8a3D7fF433252c')
+    // await expect((await tx.wait()).status).equal(1)
+
+
+    // tx = await addressList.addDeveloper('0xCCb632cdC419500015bc3fDad6335e348f007a76')
+    // await expect((await tx.wait()).status).equal(1)
+
+    // // let tx2 = await didProxy.transferOwnership("0xC2ED445Ee8DC5791662C574CE21dA57Ba5F2c79f")
+    // let tx2 = await didProxy.transferOwnership("0x8054b6F9EDf88b36BE2431f01E8a3D7fF433252c")
+    // await expect((await tx2.wait()).status).equal(1)
+
+    // tx = await deployer.sendTransaction({
+    //     to: '0x8054b6F9EDf88b36BE2431f01E8a3D7fF433252c',
+    //     value: ethers.utils.parseEther("500000000") // 5亿
+    // })
+    // await expect((await tx.wait()).status).equal(1)
+
+    // tx = await deployer.sendTransaction({
+    //     to: '0xCCb632cdC419500015bc3fDad6335e348f007a76',
+    //     value: ethers.utils.parseEther("2000000000") // 20亿
+    // })
+    // await expect((await tx.wait()).status).equal(1)
+
+
+
     // We also save the contract's artifacts and address in the frontend directory
-    saveFrontendFiles(didProxy);
+    // saveFrontendFiles(didProxy);
 }
 
-function saveFrontendFiles(didProxy) {
-    const fs = require("fs");
-    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+// function saveFrontendFiles(didProxy) {
+//     const fs = require("fs");
+//     const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
 
-    if (!fs.existsSync(contractsDir)) {
-        fs.mkdirSync(contractsDir);
-    }
+//     if (!fs.existsSync(contractsDir)) {
+//         fs.mkdirSync(contractsDir);
+//     }
 
-    fs.writeFileSync(
-        path.join(contractsDir, "contract-address.json"),
-        JSON.stringify({ DidProxy: didProxy.address }, undefined, 2)
-    );
+//     fs.writeFileSync(
+//         path.join(contractsDir, "contract-address.json"),
+//         JSON.stringify({ DidProxy: didProxy.address }, undefined, 2)
+//     );
 
-    const DidProxyArtifact = artifacts.readArtifactSync("DidProxy");
+//     const DidProxyArtifact = artifacts.readArtifactSync("DidProxy");
 
-    fs.writeFileSync(
-        path.join(contractsDir, "DidProxy.json"),
-        JSON.stringify(DidProxyArtifact, null, 2)
-    );
-}
+//     fs.writeFileSync(
+//         path.join(contractsDir, "DidProxy.json"),
+//         JSON.stringify(DidProxyArtifact, null, 2)
+//     );
+// }
 
 main()
     .then(() => process.exit(0))
